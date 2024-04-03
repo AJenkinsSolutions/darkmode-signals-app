7ccc72f477befb5a7991bf31ad04fdf02f4e6090
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-04-02 21:44:43 -0500

    Add theme service to header comp

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { ThemeService } from '../../../services/theme.service';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSlideToggleModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+
+  themeService: ThemeService = inject(ThemeService);
+
+  toggleTheme(){
+    this.themeService.updateTheme()
+  }
 }
